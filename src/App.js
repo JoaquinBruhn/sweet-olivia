c2533703fddcbbd0df300f73e8f36353e6edd877
Joaquin Bruhn <joaquin_bruhn@hotmail.com.ar>
2022-08-04 21:33:20 -0300

    Project start, created the basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/navbar";
+import Home from "./components/home/home";
+import AboutUs from "./components/aboutUs/aboutUs";
+import HowTo from "./components/howTo/howTo";
+import ProductList from "./components/catalogue/catalogueView/productList";
+
+import Footer from "./components/footer/footer";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/nosotros" element={<AboutUs />}></Route>
+          <Route exact path="/comoComprar" element={<HowTo />}></Route>
+          <Route exact path="/catalogo" element={<ProductList />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/data/productData.js
@@ -0,0 +1,82 @@
+const prodList = [
+  {
+    title: "",
+    category: "",
+    size: "",
+    price: "",
+    picture: {
+      mobile: "",
+      tablet: "",
+      desktop: "",
+    },
+    description: "",
+    stock: "",
+  },
+  {
+    title: "",
+    category: "",
+    size: "",
+    price: "",
+    picture: {
+      mobile: "",
+      tablet: "",
+      desktop: "",
+    },
+    description: "",
+    stock: "",
+  },
+  {
+    title: "",
+    category: "",
+    size: "",
+    price: "",
+    picture: {
+      mobile: "",
+      tablet: "",
+      desktop: "",
+    },
+    description: "",
+    stock: "",
+  },
+  {
+    title: "",
+    category: "",
+    size: "",
+    price: "",
+    picture: {
+      mobile: "",
+      tablet: "",
+      desktop: "",
+    },
+    description: "",
+    stock: "",
+  },
+  {
+    title: "",
+    category: "",
+    size: "",
+    price: "",
+    picture: {
+      mobile: "",
+      tablet: "",
+      desktop: "",
+    },
+    description: "",
+    stock: "",
+  },
+  {
+    title: "",
+    category: "",
+    size: "",
+    price: "",
+    picture: {
+      mobile: "",
+      tablet: "",
+      desktop: "",
+    },
+    description: "",
+    stock: "",
+  },
+];
+
+export default prodList;
