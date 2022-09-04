fad0023510dd7cb3432b4755c699f9c9c226aa24
Joaquin Bruhn <joaquin_bruhn@hotmail.com.ar>
2022-09-04 17:26:48 -0300

    added the shades to the home items and a slider along with abit of rsponsiveness to the detailed view

--- src/components/catalogue/productDetail/productDetail.js
+++ src/components/catalogue/productDetail/productDetail.js
@@ -1,49 +1,76 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { getProduct } from "../../../data/productData";
-import "./productDetail.css";
 import Whatsapp from "../../../mini-components/Whatsapp.js";
-import { useState } from "react";
+import { Container, Row, Col, Carousel } from "react-bootstrap";
+import "./productDetail.css";
 
 const ProductDetail = () => {
   const { itemTitle } = useParams();
   const product = getProduct(itemTitle);
 
   //useState(getProduct.size[0])
-  const [size,setSize]= useState(product.size[0])
-  const [color, setColor] = useState(product.color[0].colorName)
-
+  const [size, setSize] = useState(product.size[0]);
+  const [color, setColor] = useState(product.color[0].colorName);
 
   return (
-    <div className="d-flex container my-5">
-      <img className="col-6" src={product.picture.desktop} alt={product.title}></img>
-
-      <div className="productInfo d-flex flex-column col-6 py-2">
-        <h2 className="text-left ">{product.title}</h2>
-        <h2 className="text-left ">${product.price}</h2>
-        <div>
-          <p className="align-left ">TALLE</p>
-          <div className="d-flex">
-            {product.size.map((el) => {
+    <Container>
+      <Row className="d-flex my-5" xs={1} lg={2}>
+        <Col>
+          <Carousel>
+            {[product.picture.desktop, product.picture.tablet, product.picture.mobile].map((el) => {
               return (
-                <div className={ size === el ? "sampleCircle selectedCircle" : "sampleCircle"} onClick={() => {setSize(el)}} key={el}>
-                  <p>{el}</p>
-                </div>
+                <Carousel.Item key={el}>
+                  <img className="d-block w-100" src={el} alt={el} />
+                </Carousel.Item>
               );
             })}
+          </Carousel>
+        </Col>
+
+        <Col className="productInfo d-flex flex-column ">
+          <h2 className="text-left ">{product.title}</h2>
+          <h2 className="text-left ">${product.price}</h2>
+          <div>
+            <p className="align-left ">TALLE</p>
+            <div className="d-flex">
+              {product.size.map((el) => {
+                return (
+                  <div
+                    className={size === el ? "sampleCircle selectedCircle" : "sampleCircle"}
+                    onClick={() => {
+                      setSize(el);
+                    }}
+                    key={el}
+                  >
+                    <p>{el}</p>
+                  </div>
+                );
+              })}
+            </div>
           </div>
-        </div>
-        <div>
-          <p className="align-left ">COLORES</p>
-          <div className="d-flex">
-            {product.color.map((el) => {
-              return <div className={ color === el.colorName ? "sampleCircle selectedCircle" : "sampleCircle" } onClick={() => {setColor(el.colorName)}} style={{ backgroundColor: el.colorValue }} key={el.colorName}></div>;
-            })}
+          <div>
+            <p className="align-left ">COLORES</p>
+            <div className="d-flex">
+              {product.color.map((el) => {
+                return (
+                  <div
+                    className={color === el.colorName ? "sampleCircle selectedCircle" : "sampleCircle"}
+                    onClick={() => {
+                      setColor(el.colorName);
+                    }}
+                    style={{ backgroundColor: el.colorValue }}
+                    key={el.colorName}
+                  ></div>
+                );
+              })}
+            </div>
           </div>
-        </div>
-        <p className="text-left ">{product.description}</p>
-        <Whatsapp title={product.title} size={size} color={color}/>
-      </div>
-    </div>
+          <p className="text-left ">{product.description}</p>
+          <Whatsapp title={product.title} size={size} color={color} />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- src/data/productData.js
+++ src/data/productData.js
@@ -10,8 +10,10 @@ const productData = [
     ],
     price: 11,
     picture: {
-      mobile: "",
-      tablet: "",
+      mobile:
+        "https://media.istockphoto.com/id/471188329/es/foto/camiseta-roja-frontal-en-blanco-con-trazado-de-recorte.webp?s=612x612&w=is&k=20&c=f6oiR817cYR1sOVTRPwA58w4P3r0Ar_3x0hafQKWMpM=",
+      tablet:
+        "https://media.istockphoto.com/id/471188329/es/foto/camiseta-roja-frontal-en-blanco-con-trazado-de-recorte.webp?s=612x612&w=is&k=20&c=f6oiR817cYR1sOVTRPwA58w4P3r0Ar_3x0hafQKWMpM=",
       desktop:
         "https://media.istockphoto.com/id/471188329/es/foto/camiseta-roja-frontal-en-blanco-con-trazado-de-recorte.webp?s=612x612&w=is&k=20&c=f6oiR817cYR1sOVTRPwA58w4P3r0Ar_3x0hafQKWMpM=",
     },
@@ -29,8 +31,10 @@ const productData = [
     ],
     price: 22,
     picture: {
-      mobile: "",
-      tablet: "",
+      mobile:
+        "https://media.istockphoto.com/id/938486228/es/foto/jeans-casuales-pantalones-aislado-sobre-fondo-blanco.webp?s=612x612&w=is&k=20&c=RzFcu9UiXRHvTZVy3tq_Q-yLeAdu_bNDdBNJ-4dSuiE=",
+      tablet:
+        "https://media.istockphoto.com/id/938486228/es/foto/jeans-casuales-pantalones-aislado-sobre-fondo-blanco.webp?s=612x612&w=is&k=20&c=RzFcu9UiXRHvTZVy3tq_Q-yLeAdu_bNDdBNJ-4dSuiE=",
       desktop:
         "https://media.istockphoto.com/id/938486228/es/foto/jeans-casuales-pantalones-aislado-sobre-fondo-blanco.webp?s=612x612&w=is&k=20&c=RzFcu9UiXRHvTZVy3tq_Q-yLeAdu_bNDdBNJ-4dSuiE=",
     },
@@ -49,8 +53,10 @@ const productData = [
     ],
     price: 33,
     picture: {
-      mobile: "",
-      tablet: "",
+      mobile:
+        "https://media.istockphoto.com/id/1194335315/es/foto/calcetines-de-invierno-acogedores-y-c%C3%A1lidos-aislados-sobre-fondo-blanco.webp?s=612x612&w=is&k=20&c=DMrSlJcDHhxsL07LPZFFYVLfLdWL6CQHOE4JfOOwTnI=",
+      tablet:
+        "https://media.istockphoto.com/id/1194335315/es/foto/calcetines-de-invierno-acogedores-y-c%C3%A1lidos-aislados-sobre-fondo-blanco.webp?s=612x612&w=is&k=20&c=DMrSlJcDHhxsL07LPZFFYVLfLdWL6CQHOE4JfOOwTnI=",
       desktop:
         "https://media.istockphoto.com/id/1194335315/es/foto/calcetines-de-invierno-acogedores-y-c%C3%A1lidos-aislados-sobre-fondo-blanco.webp?s=612x612&w=is&k=20&c=DMrSlJcDHhxsL07LPZFFYVLfLdWL6CQHOE4JfOOwTnI=",
     },
@@ -68,8 +74,10 @@ const productData = [
     ],
     price: 44,
     picture: {
-      mobile: "",
-      tablet: "",
+      mobile:
+        "https://media.istockphoto.com/id/1317905567/es/foto/maqueta-de-sudadera-con-capucha-de-lana-con-cremallera-en-la-vista-frontal.webp?s=612x612&w=is&k=20&c=64jaS3PGBLvBGwHAHW5qLxHky8YY8CTxUpMQ4BPkzfw=",
+      tablet:
+        "https://media.istockphoto.com/id/1317905567/es/foto/maqueta-de-sudadera-con-capucha-de-lana-con-cremallera-en-la-vista-frontal.webp?s=612x612&w=is&k=20&c=64jaS3PGBLvBGwHAHW5qLxHky8YY8CTxUpMQ4BPkzfw=",
       desktop:
         "https://media.istockphoto.com/id/1317905567/es/foto/maqueta-de-sudadera-con-capucha-de-lana-con-cremallera-en-la-vista-frontal.webp?s=612x612&w=is&k=20&c=64jaS3PGBLvBGwHAHW5qLxHky8YY8CTxUpMQ4BPkzfw=",
     },
@@ -87,8 +95,10 @@ const productData = [
     ],
     price: 55,
     picture: {
-      mobile: "",
-      tablet: "",
+      mobile:
+        "https://media.istockphoto.com/id/1366255386/es/vector/zapatillas-medias.webp?s=612x612&w=is&k=20&c=qz_XsdKjbAxVmpl9ogP-No23DpEJOGI7pZRXx76ZZ8Y=",
+      tablet:
+        "https://media.istockphoto.com/id/1366255386/es/vector/zapatillas-medias.webp?s=612x612&w=is&k=20&c=qz_XsdKjbAxVmpl9ogP-No23DpEJOGI7pZRXx76ZZ8Y=",
       desktop:
         "https://media.istockphoto.com/id/1366255386/es/vector/zapatillas-medias.webp?s=612x612&w=is&k=20&c=qz_XsdKjbAxVmpl9ogP-No23DpEJOGI7pZRXx76ZZ8Y=",
     },
@@ -106,8 +116,10 @@ const productData = [
     ],
     price: 66,
     picture: {
-      mobile: "",
-      tablet: "",
+      mobile:
+        "https://media.istockphoto.com/id/637237414/es/foto/bufanda-a-rayas-blancas-gris-lana-sobre-blanco.webp?s=612x612&w=is&k=20&c=A_PWcDADZNTiH3JJWpp8mM0SJN2v2Jt-cArUOgZX95k=",
+      tablet:
+        "https://media.istockphoto.com/id/637237414/es/foto/bufanda-a-rayas-blancas-gris-lana-sobre-blanco.webp?s=612x612&w=is&k=20&c=A_PWcDADZNTiH3JJWpp8mM0SJN2v2Jt-cArUOgZX95k=",
       desktop:
         "https://media.istockphoto.com/id/637237414/es/foto/bufanda-a-rayas-blancas-gris-lana-sobre-blanco.webp?s=612x612&w=is&k=20&c=A_PWcDADZNTiH3JJWpp8mM0SJN2v2Jt-cArUOgZX95k=",
     },
@@ -126,8 +138,10 @@ const productData = [
     ],
     price: 77,
     picture: {
-      mobile: "",
-      tablet: "",
+      mobile:
+        "https://media.istockphoto.com/id/656204400/es/foto/guantes-de-cuero-negro.webp?s=612x612&w=is&k=20&c=2aDvHQIcgJLChn03nzp5Q-WQ74XFBzsvOsYsBtIXgDQ=",
+      tablet:
+        "https://media.istockphoto.com/id/656204400/es/foto/guantes-de-cuero-negro.webp?s=612x612&w=is&k=20&c=2aDvHQIcgJLChn03nzp5Q-WQ74XFBzsvOsYsBtIXgDQ=",
       desktop:
         "https://media.istockphoto.com/id/656204400/es/foto/guantes-de-cuero-negro.webp?s=612x612&w=is&k=20&c=2aDvHQIcgJLChn03nzp5Q-WQ74XFBzsvOsYsBtIXgDQ=",
     },
@@ -145,8 +159,10 @@ const productData = [
     ],
     price: 88,
     picture: {
-      mobile: "",
-      tablet: "",
+      mobile:
+        "https://media.istockphoto.com/id/1320317354/es/foto/cintur%C3%B3n-vaquero-de-cuero-sobre-fondo-de-madera.webp?s=612x612&w=is&k=20&c=O00u8b-xmersX6NQjs1Gs0wi2gZP6worJb_3uolNRc4=",
+      tablet:
+        "https://media.istockphoto.com/id/1320317354/es/foto/cintur%C3%B3n-vaquero-de-cuero-sobre-fondo-de-madera.webp?s=612x612&w=is&k=20&c=O00u8b-xmersX6NQjs1Gs0wi2gZP6worJb_3uolNRc4=",
       desktop:
         "https://media.istockphoto.com/id/1320317354/es/foto/cintur%C3%B3n-vaquero-de-cuero-sobre-fondo-de-madera.webp?s=612x612&w=is&k=20&c=O00u8b-xmersX6NQjs1Gs0wi2gZP6worJb_3uolNRc4=",
     },
