688bb9670b0037924f178866037c86f2ec42de8a
Joaquin Bruhn <joaquin_bruhn@hotmail.com.ar>
2022-09-13 16:22:39 -0300

    added a dropdown for the categories on the mobile view of productList

--- src/components/catalogue/catalogueView/productList.js
+++ src/components/catalogue/catalogueView/productList.js
@@ -1,6 +1,7 @@
 import ProductCard from "./productCard/ProductCard";
 import productData from "../../../data/productData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Dropdown } from "react-bootstrap";
 import SearchBar from "../../searchBar/SearchBar.js";
 import { cats } from "../../../data/productData";
 import "./productList.css";
@@ -8,6 +9,17 @@ import "./productList.css";
 const ProductList = () => {
   const [category, setCategory] = useState("Todos");
   const [search, setSearch] = useState(productData);
+  const [width, setWidth] = useState(window.innerWidth);
+  const breakpoint = 540;
+
+  useEffect(() => {
+    const handleResizedWindow = () => setWidth(window.innerWidth);
+
+    window.addEventListener("resize", handleResizedWindow);
+    return () => {
+      window.removeEventListener("resize", handleResizedWindow);
+    };
+  }, [width, setWidth]);
 
   const categoryFilter = () => {
     if (category === "Todos") {
@@ -20,21 +32,44 @@ const ProductList = () => {
 
   return (
     <div className="catalogue">
-      <div className="listFilter ">
-        {cats.map((el) => {
-          return (
-            <p
-              key={el}
-              className="filter"
-              onClick={() => {
-                setCategory(el);
-              }}
-            >
-              {el}
-            </p>
-          );
-        })}
-      </div>
+      {width > breakpoint ? (
+        <div className="listFilter">
+          {cats.map((el) => {
+            return (
+              <p
+                key={el}
+                className="filter"
+                onClick={() => {
+                  setCategory(el);
+                }}
+              >
+                {el}
+              </p>
+            );
+          })}
+        </div>
+      ) : (
+        <Dropdown className="catsDropdown">
+          <Dropdown.Toggle variant="dark" id="dropdown-basic">
+            Categorias
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            {cats.map((el) => {
+              return (
+                <Dropdown.Item
+                  key={el}
+                  className="filter"
+                  onClick={() => {
+                    setCategory(el);
+                  }}
+                >
+                  {el}
+                </Dropdown.Item>
+              );
+            })}
+          </Dropdown.Menu>
+        </Dropdown>
+      )}
       <SearchBar search={search} setSearch={setSearch} />
       <div className="catalogueList container my-5">
         {categoryFilter().length > 0 ? (
@@ -52,3 +87,20 @@ const ProductList = () => {
 };
 
 export default ProductList;
+
+// <div className="catalogue">
+//   <div className="listFilter ">
+//     {cats.map((el) => {
+//       return (
+//         <p
+//           key={el}
+//           className="filter"
+//           onClick={() => {
+//             setCategory(el);
+//           }}
+//         >
+//           {el}
+//         </p>
+//       );
+//     })}
+//   </div>
