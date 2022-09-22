9375e70af5a91f6067816a6c9801444dd6602f9b
desarrollolc <lucianonicolas10@gmail.com>
2022-09-22 12:33:19 -0300

    new products maded, first final stylization

---
+++ src/data/catalogue/productBikinis.js
@@ -0,0 +1,23 @@
+const bikinis = [
+  {
+    title: "Bikinis",
+    category: "Bikinis",
+    size: ["XS", "S", "M"],
+    color: [
+      { colorName: "Verde", colorValue: "#6BFF33" },
+      { colorName: "Rojo", colorValue: "#F20505" },
+      { colorName: "Celeste", colorValue: "#05F2EB" },
+    ],
+    price: 11,
+    discount: false,
+    picture: [
+      "https://media.istockphoto.com/id/471188329/es/foto/camiseta-roja-frontal-en-blanco-con-trazado-de-recorte.webp?s=612x612&w=is&k=20&c=f6oiR817cYR1sOVTRPwA58w4P3r0Ar_3x0hafQKWMpM=",
+      "https://media.istockphoto.com/id/471188329/es/foto/camiseta-roja-frontal-en-blanco-con-trazado-de-recorte.webp?s=612x612&w=is&k=20&c=f6oiR817cYR1sOVTRPwA58w4P3r0Ar_3x0hafQKWMpM=",
+      "https://media.istockphoto.com/id/471188329/es/foto/camiseta-roja-frontal-en-blanco-con-trazado-de-recorte.webp?s=612x612&w=is&k=20&c=f6oiR817cYR1sOVTRPwA58w4P3r0Ar_3x0hafQKWMpM=",
+    ],
+    description: "Lorem",
+    stock: 13,
+  },
+];
+
+export default bikinis;
